76b7738f9f0e2ec73c967bb889960c009e1c6ad8
vitasenka <piroozamirpour@gmail.com>
2025-09-28 17:40:28 +0330

    operators part 1

---
+++ main.py
@@ -0,0 +1,26 @@
+# 1. Operators
+
+# 1.1 Arithmetic
+a = 7
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b )
+print(a // b) # integer division, floor
+print(a % b)  # remainder
+print(a ** b)
+
+# 1.2 Assignment
+x = 10
+x += 2
+print(x)
+x *= 3
+print(x)
+
+# 1.3 Comparison
+w, h, d = 20, 10, 5
+print(w == 20)
+print(h != 20)
+print(w > h)
+print(d <= 5)
